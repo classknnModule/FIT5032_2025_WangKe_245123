36c2dd9384caa075b5ac35d1ad1c2d5cef31a517
classknnModule <kwan0142@student.monash.edu>
2025-07-11 11:35:05 +0800

    Assessed Lab 3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import './assets/main.css'
+// import './assets/main.css'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
-// import App from './App.vue'
-import JSON from './components/JSON.vue'
+import App from './App.vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import JSON from './components/JSON.vue'
 
-// createApp(App).mount('#app')
-createApp(JSON).mount('#app')
+createApp(App).mount('#app')
+// createApp(JSON).mount('#app')
